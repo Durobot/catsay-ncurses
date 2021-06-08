06acfbdba37cec9c0e11afe5d35d9b8ac55f965f
Alexei Kireev <durobotdev@gmail.com>
2021-06-08 15:45:56 +0300

    First working version, with ESC key handling

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A variation of catsay-tui project, as found in the book "Practical Rust Projects Building Game, Physical Computing, and Machine Learning Applications" by Shing Lyu.
 
 The original version of this project (and other projects from the book) is at https://github.com/apress/practical-rust-projects
+
+In order to build this project, you must have the development version of **ncurses** library installed. On Ubuntu it can be installed with `sudo apt install libncursesw5-dev`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+//use cursive::Cursive;
+use cursive::views::TextView;
+use cursive::event::Key;
+
+fn main()
+{
+    // Creates the cursive root - required for every application.
+    let mut siv = cursive::default();
+    let cat_text = "Meow!
+ \\
+  \\
+   /\\_/\\
+  ( o o )
+  =( I )=";
+    // Declaring the app layout
+    siv.add_layer(TextView::new(cat_text));
+    // Listen to Key::Esc and quit
+    siv.add_global_callback(Key::Esc, |s| s.quit());
+    siv.run(); // starting the event loop
 }
